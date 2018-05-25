370843d1b0bb08c222472cb0b82c4d02c2e0ac6c
TAJORE <jouanirenatot@gmail.com>
2018-05-25 13:51:43 +0200

    eloquent relationships and comments

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
-    <link href="css/blog.css" rel="stylesheet">
+    <link href="css/app.scss" rel="stylesheet">
 </head>
 
 <body>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,29 +8,22 @@
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 <!-- Custom styles for this template -->
-<link href="css/blog.css" rel="stylesheet">
-<body>
+<link href="css/app.css" rel="stylesheet">
+<body>h
 <div class="container">
     <div class="row">
-
         <div class="col-sm-8 blog-main">
             @foreach ($posts as $post)
-
                 @include ('posts.post')
-
             @endforeach
             <nav>
                 <ul class="pager">
-                    <li><a href="#">Previous</a></li>
-                    <li><a href="#">Next</a></li>
+                    <li><a href="#">Old</a></li>
+                    <li><a href="#">New</a></li>
                 </ul>
             </nav>
-
         </div><!-- /.blog-main -->
-
-
     </div><!-- /.row -->
-
 </div><!-- /.container -->
 
 </body>
