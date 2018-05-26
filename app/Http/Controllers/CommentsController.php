7abd12d85988aac5ef7d115798a3917cca9ac380
TAJORE <jouanirenatot@gmail.com>
2018-05-26 17:24:15 +0200

    add comment

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -10,8 +10,11 @@ class CommentsController extends Controller
     public function store(Post $post)
     {
 
-        $this->validate(request(), ['boy'=>'required|min:2']);
+        $this->validate(request(), ['body' => 'required|min:2']);
+
+
         $post->addComment(request('body'));
+
         return back();
 
     }
