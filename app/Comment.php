36c01b567e6c4e613614db7f3c3ba96197e65590
TAJORE <jouanirenatot@gmail.com>
2018-05-31 13:17:53 +0200

    associating with users

--- app/Comment.php
+++ app/Comment.php
@@ -13,6 +13,9 @@ class Comment extends Model
     public function post(){
         return $this->belongsTo(Post::class);
     }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 
 
 
--- app/Post.php
+++ app/Post.php
@@ -10,6 +10,9 @@ class Post extends Model
     {
         return $this->hasMany(Comment::class);
     }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
     protected $fillable = [
         'title', 'body'
     ];
